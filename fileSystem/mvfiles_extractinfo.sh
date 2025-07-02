5f77fe3f65cf5f3a1734033dcb519c35871706de
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-07-02 13:04:29 -0500

    add script to move and extract info from .zip files in Downloads

---
+++ fileSystem/mvfiles_extractinfo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOWNLOADS_DIR="$HOME/Downloads"
+DEST_DIR="/Users/sebastian/dev/learning/dc-account/client-management/july"
+
+if [ ! -d "$DOWNLOADS_DIR" ]; then
+    echo "Error: Downloads directory not found: $DOWNLOADS_DIR"
+    exit 1
+fi
+
+if [ ! -d "$DEST_DIR" ]; then
+    echo "Creating destination directory: $DEST_DIR"
+    mkdir -p "$DEST_DIR"
+fi
+
+echo "Moving .zip files from $DOWNLOADS_DIR to $DEST_DIR"
+
+zip_count=0
+for zip_file in "$DOWNLOADS_DIR"/*.zip; do
+    if [ -f "$zip_file" ]; then
+        filename=$(basename "$zip_file")
+        echo "Moving: $filename"
+        mv "$zip_file" "$DEST_DIR/"
+        
+        if [ $? -eq 0 ]; then
+            echo "Successfully moved: $filename"
+            ((zip_count++))
+            
+            echo "Extracting info from: $filename"
+            unzip -l "$DEST_DIR/$filename" | head -20
+            echo "---"
+        else
+            echo "Failed to move: $filename"
+        fi
+    fi
+done
+
+if [ $zip_count -eq 0 ]; then
+    echo "No .zip files found in $DOWNLOADS_DIR"
+else
+    echo "Moved $zip_count .zip file(s) to $DEST_DIR"
+fi
